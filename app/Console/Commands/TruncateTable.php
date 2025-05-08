383c82b23ef0c5064b5d7b5bcd410a13e51899e5
akildevs1000 <akildevs1000@gmail.com>
2025-05-08 17:09:38 +0400

    test

--- app/Console/Commands/TruncateTable.php
+++ app/Console/Commands/TruncateTable.php
@@ -3,12 +3,14 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class TruncateTable extends Command
 {
     protected $signature = 'table:truncate {table}';
+
     protected $description = 'Truncate the given database table';
 
     public function handle()
@@ -23,6 +25,10 @@ class TruncateTable extends Command
         try {
             DB::table($table)->truncate();
             $this->info("Table '{$table}' truncated successfully.");
+
+            Cache::forget('order_stats_last_month');
+            Cache::forget('invoice_stats_last_month');
+
             return Command::SUCCESS;
         } catch (\Exception $e) {
             $this->error("Failed to truncate table '{$table}': " . $e->getMessage());
