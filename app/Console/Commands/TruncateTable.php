8c8c778f0a6d1a77e22d12bfad6f2b32db964fce
akildevs1000 <akildevs1000@gmail.com>
2025-05-08 17:02:02 +0400

    test

---
+++ app/Console/Commands/TruncateTable.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class TruncateTable extends Command
+{
+    protected $signature = 'table:truncate {table}';
+    protected $description = 'Truncate the given database table';
+
+    public function handle()
+    {
+        $table = $this->argument('table');
+
+        if (!Schema::hasTable($table)) {
+            $this->error("Table '{$table}' does not exist.");
+            return Command::FAILURE;
+        }
+
+        try {
+            DB::table($table)->truncate();
+            $this->info("Table '{$table}' truncated successfully.");
+            return Command::SUCCESS;
+        } catch (\Exception $e) {
+            $this->error("Failed to truncate table '{$table}': " . $e->getMessage());
+            return Command::FAILURE;
+        }
+    }
+}
