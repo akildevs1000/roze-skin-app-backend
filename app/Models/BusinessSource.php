e1727d1c348dff4263408864832e3125d8ab9d61
akildevs1000 <akildevs1000@gmail.com>
2025-05-29 12:42:50 +0400

    added order by udpated desc for business source and delivery service

--- app/Models/BusinessSource.php
+++ app/Models/BusinessSource.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\OrderByUpdatedAt;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,22 @@ class BusinessSource extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected static function booted()
+    {
+        // Disable in Controller if Needed in =>  YourModel::withoutGlobalScope(OrderByUpdatedAt::class)->get();
+        static::addGlobalScope(new OrderByUpdatedAt);
+    }
+
+    protected static function booted_old()
+    {
+        // in case want to use key to disable on contorller level
+        // static::addGlobalScope('orderByUpdatedAt', function (Builder $builder) {
+        //     $builder->orderBy('updated_at', 'desc');
+        // });
+
+        // example usage in controller to disable
+        // YourModel::withoutGlobalScope('orderByUpdatedAt')->get();
+
+    }
 }
--- app/Models/DeliveryService.php
+++ app/Models/DeliveryService.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\OrderByUpdatedAt;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,10 @@ class DeliveryService extends Model
     use HasFactory;
     
     protected $guarded = [];
+
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new OrderByUpdatedAt);
+    }
 }
