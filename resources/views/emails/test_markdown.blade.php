c536fe613b2518e73d6f646ed48618be91fe9430
akildevs1000 <akildevs1000@gmail.com>
2025-07-09 12:57:26 +0400

    test

--- resources/views/emails/test_markdown.blade.php
+++ resources/views/emails/test_markdown.blade.php
@@ -1,17 +1,10 @@
 @component('mail::message')
-# Hello!
+# Dear Customer!
 
-This is a **test email** sent using **Markdown** formatting.
+The status of your shipment has been updated.
 
-- You can use bullet lists
-- **Bold text**
-- _Italic text_
-- [Links](https://rozeskin.com/tracking/?tracking_id=5100308838)
-
-@component('mail::button', ['url' => 'https://rozeskin.com/tracking/?tracking_id=5100308838'])
-Visit Laravel
-@endcomponent
+[Track your shipment here](https://rozeskin.com/tracking/?tracking_id=5100308838)
 
 Thanks,<br>
-{{ config('app.name') }}
+RozeSkin
 @endcomponent
