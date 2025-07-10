87c48b012ff35ce7ce42e028e65e6a35c7168508
akildevs1000 <akildevs1000@gmail.com>
2025-07-10 11:54:26 +0400

    test

--- config/app.php
+++ config/app.php
@@ -196,6 +196,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         TechTailor\RPG\RPGServiceProvider::class,
+        App\Providers\MailConfigServiceProvider::class,
     ],
 
     /*
