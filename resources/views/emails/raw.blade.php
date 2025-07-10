de60fc592fc67c6c3673f9afaa85a411f002c161
akildevs1000 <akildevs1000@gmail.com>
2025-07-10 12:12:00 +0400

    removed exrta mail files

--- resources/views/emails/raw.blade.php
+++
@@ -1,9 +0,0 @@
-<p>Dear Customer,</p>
-<p>The status of your shipment has been updated.</p>
-<p>
-    Track your shipment here:
-    <a href="https://rozeskin.com/tracking/?tracking_id=5100308207">
-        https://rozeskin.com/tracking/?tracking_id=5100308207
-    </a>
-</p>
-<p>Thank you,<br>Rozeskin Team</p>
