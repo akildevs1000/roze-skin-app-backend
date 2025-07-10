93f4a5441c3061f67657999cdc94f6f1a9861c05
akildevs1000 <akildevs1000@gmail.com>
2025-07-10 17:38:40 +0400

    update kernel.php for cron jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,71 +11,14 @@ use Illuminate\Support\Facades\Mail;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
         $schedule
             ->command('track:shipment')
             ->hourly()
             ->emailOutputOnFailure(env("ADMIN_MAIL_RECEIVERS"));
-
-        $schedule
-            ->command('app:process-audit-freeze')
-            ->dailyAt('00:30')
-            ->emailOutputOnFailure(env("ADMIN_MAIL_RECEIVERS"));
-
-        $schedule
-            ->command('task:generate_reports')
-            ->dailyAt('1:00')
-            ->emailOutputOnFailure(env("ADMIN_MAIL_RECEIVERS"));
-
-        $schedule
-            ->command('task:db_backup')
-            ->dailyAt('3:00')
-            ->emailOutputOnFailure(env("ADMIN_MAIL_RECEIVERS"));
-
-        $schedule
-            ->command('send:audit_report')
-            // ->everyFiveMinutes()
-            ->dailyAt('9:00')
-            ->emailOutputOnFailure(env("ADMIN_MAIL_RECEIVERS"));
-
-
-        // generate records in background for report
-
-        $companyIds = Company::where("is_background_jobs", true)->pluck("id");
-
-        foreach ($companyIds as $companyId) {
-            $schedule
-                ->command("record:generate-daily-summary $companyId")
-                // ->everyFiveMinutes()
-                ->dailyAt('1:30')
-                ->emailOutputOnFailure(env("ADMIN_MAIL_RECEIVERS"));
-
-            $schedule
-                ->command("record:generate-daily-cash $companyId")
-                // ->everyFiveMinutes()
-                ->dailyAt('1:45')
-                ->emailOutputOnFailure(env("ADMIN_MAIL_RECEIVERS"));
-
-            $schedule
-                ->command("record:generate-daily-ota $companyId")
-                // ->everyFiveMinutes()
-                ->dailyAt('2:00')
-                ->emailOutputOnFailure(env("ADMIN_MAIL_RECEIVERS"));
-        }
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
         $this->load(__DIR__ . '/Commands');
