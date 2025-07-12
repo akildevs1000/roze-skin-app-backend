98cf04eaa42c70d7bf85987d96e1db8a74c45245
akildevs1000 <akildevs1000@gmail.com>
2025-07-12 12:04:12 +0400

    disabled test command for track shippment

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,9 +9,9 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
-        $schedule
-            ->command('track:shipments-test')
-            ->everyMinute();
+        // $schedule
+        //     ->command('track:shipments-test')
+        //     ->everyMinute();
 
         $schedule
             ->command('track:shipments')
